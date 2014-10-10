ef3fc6fe570973289faebe64e7b487e967b9e1f9
Jacob Pålsson <jacpa975@student.liu.se>
2014-10-10 20:36:21 +0000

    asdlmö

--- touch/main.c
+++ touch/main.c
@@ -2,21 +2,21 @@
 #include <SPI.h>
 #include <Wire.h>
 
-#include "Adafruit_STMPE610.h"
+#include "STMPE610.h"
 #define STMPE_CS 10
 
 void writeRegister8(uint8_t reg, uint8_t val) {
-    if (_CLK == -1){
-     	SPI.beginTransaction(mySPISettings);
- 	}
-    digitalWrite(_CS, LOW);
-    spiOut(reg); 
-    spiOut(val);
-    digitalWrite(_CS, HIGH);
- 	if (_CLK == -1){
- 		SPI.endTransaction();
- 	}
-  }
+	if (_CLK == -1){
+		SPI.beginTransaction(mySPISettings);
+	}
+	digitalWrite(_CS, LOW);
+	spiOut(reg); 
+	spiOut(val);
+	digitalWrite(_CS, HIGH);
+	if (_CLK == -1){
+		SPI.endTransaction();
+	}
+}
 }
 
 
@@ -26,43 +26,43 @@ void Adafruit_STMPE610(uint8_t cspin) {
   	SPI.begin();	// fixa
   	m_spiMode = SPI_MODE0; //fixa
   	initSPI(); // mySPISettings = SPISettings(1000000, MSBFIRST, SPI_MODE0); // fixa
- }
-
- 
-
-void begin(uint8_t i2caddr) {
-  if (_CS != -1 && _CLK == -1) {
-    // hardware SPI
-    pinMode(_CS, OUTPUT);
-    digitalWrite(_CS, HIGH);
-    SPI.begin();
-    mySPISettings = SPISettings(1000000, MSBFIRST, SPI_MODE0);
-
-    m_spiMode = SPI_MODE0;
   }
+
+
+
+  void begin(uint8_t i2caddr) {
+  	if (_CS != -1 && _CLK == -1) {
+    // hardware SPI
+  		pinMode(_CS, OUTPUT);
+  		digitalWrite(_CS, HIGH);
+  		SPI.begin();
+  		mySPISettings = SPISettings(1000000, MSBFIRST, SPI_MODE0);
+
+  		m_spiMode = SPI_MODE0;
+  	}
 
   // try mode0
-  if (getVersion() != 0x811) {
-    if (_CS != -1 && _CLK == -1) {
+  	if (getVersion() != 0x811) {
+  		if (_CS != -1 && _CLK == -1) {
       //Serial.println("try MODE1");
-      mySPISettings = SPISettings(1000000, MSBFIRST, SPI_MODE1);
-      m_spiMode = SPI_MODE1;
-    
-      if (getVersion() != 0x811) {
-	    return false;
-      }
-    } else {
-      return false;
-    }
-  }
+  			mySPISettings = SPISettings(1000000, MSBFIRST, SPI_MODE1);
+  			m_spiMode = SPI_MODE1;
 
-  writeRegister8(STMPE_SYS_CTRL1, STMPE_SYS_CTRL1_RESET);
-  delay(10);
-  
-  for (uint8_t i=0; i<65; i++) {
-    readRegister8(i);
-  }
-  
+  			if (getVersion() != 0x811) {
+  				return false;
+  			}
+  		} else {
+  			return false;
+  		}
+  	}
+
+  	writeRegister8(STMPE_SYS_CTRL1, STMPE_SYS_CTRL1_RESET);
+  	delay(10);
+
+  	for (uint8_t i=0; i<65; i++) {
+  		readRegister8(i);
+  	}
+
   writeRegister8(STMPE_SYS_CTRL2, 0x0); // turn on clocks!
   writeRegister8(STMPE_TSC_CTRL, STMPE_TSC_CTRL_XYZ | STMPE_TSC_CTRL_EN); // XYZ and enable!
   //Serial.println(readRegister8(STMPE_TSC_CTRL), HEX);
@@ -81,28 +81,27 @@ void begin(uint8_t i2caddr) {
 
 // kolla i main if != 0
 uint8_t touched(void) {
-  return (readRegister8(STMPE_TSC_CTRL) & 0x80);
+	return (readRegister8(STMPE_TSC_CTRL) & 0x80);
 }
 // kolla i main if != 0
 uint8_t bufferEmpty(void) {
-  return (readRegister8(STMPE_FIFO_STA) & STMPE_FIFO_STA_EMPTY);
+	return (readRegister8(STMPE_FIFO_STA) & STMPE_FIFO_STA_EMPTY);
 }
 
 uint8_t bufferSize(void) {
-  return readRegister8(STMPE_FIFO_SIZE);
+	return readRegister8(STMPE_FIFO_SIZE);
 }
 
 uint16_t getVersion() {
-  uint16_t v;
+	uint16_t v;
   //Serial.print("get version");
-  v = readRegister8(0);
-  v <<= 8;
-  v |= readRegister8(1);
+	v = readRegister8(0);
+	v <<= 8;
+	v |= readRegister8(1);
   //Serial.print("Version: 0x"); Serial.println(v, HEX);
-  return v;
+	return v;
 }
 
-void writeRegister8(uint8_t reg, uint8_t val);
 
 int main(void)
 {
@@ -114,100 +113,122 @@ int main(void)
 
 
 void readData(uint16_t *x, uint16_t *y, uint8_t *z) {
-  uint8_t data[4];
-  
-  for (uint8_t i=0; i<4; i++) {
-    data[i] = readRegister8(0xD7); //SPI.transfer(0x00); 
-   // Serial.print("0x"); Serial.print(data[i], HEX); Serial.print(" / ");
-  }
-  *x = data[0];
-  *x <<= 4;
-  *x |= (data[1] >> 4);
-  *y = data[1] & 0x0F; 
-  *y <<= 8;
-  *y |= data[2]; 
-  *z = data[3];
+	uint8_t data[4];
 
-  if (bufferEmpty())
-    writeRegister8(STMPE_INT_STA, 0xFF); // reset all ints
+	for (uint8_t i=0; i<4; i++) {
+    	data[i] = readRegister8(0xD7); //SPI.transfer(0x00); 
+   // Serial.print("0x"); Serial.print(data[i], HEX); Serial.print(" / ");
+	}
+	*x = data[0];
+	*x <<= 4;
+	*x |= (data[1] >> 4);
+	*y = data[1] & 0x0F; 
+	*y <<= 8;	
+	*y |= data[2]; 
+	*z = data[3];
+
+	if (bufferEmpty()){
+		writeRegister8(STMPE_INT_STA, 0xFF); // reset all ints
+	}
+    
 }
 
 
+
 uint8_t spiIn() {
-  if (_CLK == -1) {
-    uint8_t d = SPI.transfer(0);
-    return d;
-  }
-  else
-    return shiftIn(_MISO, _CLK, MSBFIRST);
+	if (_CLK == -1) {
+		uint8_t d = SPI.transfer(0);
+		return d;
+	}
+	else
+		return shiftIn(_MISO, _CLK, MSBFIRST);
 }
 
 
 void spiOut(uint8_t x) {  
-  if (_CLK == -1) {
-    SPI.transfer(x);
-  }
-  else
-    shiftOut(_MOSI, _CLK, MSBFIRST, x);
+	if (_CLK == -1) {
+		SPI.transfer(x);
+	}
+	else
+		shiftOut(_MOSI, _CLK, MSBFIRST, x);
 }
 
 
 
 uint8_t readRegister8(uint8_t reg) {
-  uint8_t x ;
-  
-    if (_CLK == -1){
-    	SPI.beginTransaction(mySPISettings);
-    } 
-    digitalWrite(_CS, LOW);
-    spiOut(0x80 | reg); 
-    spiOut(0x00);
-    x = spiIn(); 
-    digitalWrite(_CS, HIGH);
+	uint8_t x ;
 
-    if (_CLK == -1){
-    	SPI.endTransaction();
-    } 
+	if (_CLK == -1){
+		SPI.beginTransaction(mySPISettings);
+	} 
+	digitalWrite(_CS, LOW);
+	spiOut(0x80 | reg); 
+	spiOut(0x00);
+	x = spiIn(); 
+	digitalWrite(_CS, HIGH);
 
-  return x;
+	if (_CLK == -1){
+		SPI.endTransaction();
+	} 
+	return x;
 }
 
 
 uint16_t readRegister16(uint8_t reg) {
-  uint16_t x;
- if (_CLK == -1) {
+	uint16_t x;
+	if (_CLK == -1) {
     // hardware SPI
-    if (_CLK == -1) {
-    	SPI.beginTransaction(mySPISettings);
-    }
-    digitalWrite(_CS, LOW);
-    spiOut(0x80 | reg); 
-    spiOut(0x00);
-    x = spiIn(); 
-    x<<=8;
-    x |= spiIn(); 
-    digitalWrite(_CS, HIGH);
-    if (_CLK == -1) {
-    	SPI.endTransaction();
-    }
+		if (_CLK == -1) {
+			SPI.beginTransaction(mySPISettings);
+		}
+		digitalWrite(_CS, LOW);
+		spiOut(0x80 | reg); 
+		spiOut(0x00);
+		x = spiIn(); 
+		x<<=8;
+		x |= spiIn(); 
+		digitalWrite(_CS, HIGH);
+		if (_CLK == -1) {
+			SPI.endTransaction();
+		}
 
-  }
+	}
   //Serial.print("$"); Serial.print(reg, HEX); 
   //Serial.print(": 0x"); Serial.println(x, HEX);
-  return x;
+	return x;
 }
 
 void writeRegister8(uint8_t reg, uint8_t val) {
 
-    if (_CLK == -1) {
-    	SPI.beginTransaction(mySPISettings);
-    }
-    digitalWrite(_CS, LOW);
-    spiOut(reg); 
-    spiOut(val);
-    digitalWrite(_CS, HIGH);
-    if (_CLK == -1) {
-    	SPI.endTransaction();
-    }
- }
+	if (_CLK == -1) {
+		SPI.beginTransaction(mySPISettings);
+	}
+	digitalWrite(_CS, LOW);
+	spiOut(reg); 
+	spiOut(val);
+	digitalWrite(_CS, HIGH);
+	if (_CLK == -1) {
+		SPI.endTransaction();
+	}
+
+
+	uint16_t x, y;
+	uint8_t z;
+	if (touched()) {
+    // read x & y & z;
+		while (!bufferEmpty()) {
+			print(bufferSize());
+			readData(&x, &y, &z);
+			print("->("); 
+			print(x); 
+			print(", "); 
+			print(y); 
+			print(", "); 
+			print(z);
+			println(")");
+			}
+    	writeRegister8(STMPE_INT_STA, 0xFF); // reset all ints
+	}
+delay(10);
+}
 
